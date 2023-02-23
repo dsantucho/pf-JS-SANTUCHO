4942203ef853c932f8ccc4ed893f0c678fd62083
Daniela Santucho <daniela.santucho@fzsports.com>
2023-02-22 21:33:45 -0300

    movi las funciones del home al home.js

---
+++ script/home.js
@@ -0,0 +1,54 @@
+import * as functions from "./functions.js";
+import { itemSuper, user } from "./objetos.js";
+
+//variables
+
+//formularios
+document.addEventListener("DOMContentLoaded", function () {
+    if(!(functions.getLocalStorage("loginUser"))){
+        window.location = "../index.html";
+    }
+    functions.displayItems("itemsGrocery"); //if empty -> console.log
+    //add item in grocery
+    const groceryForm = document.getElementById("groceryForm");
+
+    groceryForm.addEventListener("submit", addItem);
+
+    function addItem(event) {
+        event.preventDefault();
+
+        let itemInput = document.getElementById("itemInput").value;
+        let itemPrice = document.getElementById("itemPrice").value;
+        const resultSpan = document.getElementById("result");
+
+        const user = functions.getLocalStorage("loginUser");
+        if (user) {
+            //save item
+            functions.saveItemSupermarket(
+                itemInput,
+                itemPrice,
+                user.idUser,
+                "itemsGrocery"
+            );
+
+            //display items
+            let buttonAdd = document.getElementById("button-add");
+            buttonAdd.addEventListener(
+                "click",
+                functions.displayItems("itemsGrocery")
+            );
+
+            //display sum
+            let result = functions.sumItemsPrice("itemsGrocery");
+            resultSpan.innerHTML = result;
+
+            //clean
+            document.getElementById("itemInput").value = "";
+            document.getElementById("itemPrice").value = "";
+        }else{
+            window.location = "../index.html";
+        }
+
+    }
+
+});
